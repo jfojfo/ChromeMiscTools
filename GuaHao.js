634754bc45fda99b4d8864ae1f58a7486d0341d3
jfosnda <jfojfo@gmail.com>
2013-12-25 13:28:59 +0800

    Add GuaHao.js

---
+++ GuaHao.js
@@ -0,0 +1,9 @@
+console.log("GuaHao");
+var guahao = $("form .guahaor");
+if (guahao.length > 0) {
+    guahao = $(guahao.get(0));
+    guahao.find("#jiuz").val("302107907");
+    guahao.find("#ybkh").val("110105133580");
+    guahao.find("#baoxiao").val("1");
+    console.log(guahao);
+}
